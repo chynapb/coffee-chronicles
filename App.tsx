c93a080f414664f112cc1d633bfb4d85a6857b74
Chyna <chynapblair@gmail.com>
2024-10-19 11:27:06 -0400

    Updated file structure and folders Updated welcome screen

--- App.tsx
+++ App.tsx
@@ -1,19 +1,33 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, StatusBar } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style='auto' />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.header}>Coffee Chronicles</Text>
+        <Text style={styles.text}>Find your perfect brew</Text>
+      </View>
+      <StatusBar barStyle='dark-content' />
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  header: {
+    fontSize: 36,
+    color: '#343450',
+  },
+  text: {
+    fontSize: 22,
+    marginTop: 15,
+    marginBottom: 75,
+    color: '#FF4500',
+    textAlign: 'center',
   },
 })
